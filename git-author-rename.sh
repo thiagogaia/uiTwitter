becf2bec28095d928f569df7bb56b12e3341ac8d
Thiago Gaia <thiagogaia@gmail.com>
2023-04-05 20:52:33 -0300

    test

---
+++ git-author-rename.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git filter-branch --env-filter 'if [ "$GIT_AUTHOR_EMAIL" = "thiago.gaia@monetizze.com.br" ]; then
+GIT_AUTHOR_EMAIL="thiagogaia@gmail.com";
+GIT_AUTHOR_NAME="Thiago Gaia"
+GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL;
+GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"; fi' -- --all
