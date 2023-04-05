39d1ed287a5da87735e2c858db4786017a0efb9f
Thiago Gaia <thiagogaia@gmail.com>
2023-04-05 20:54:35 -0300

    vai

--- git-author-rename.sh
+++ git-author-rename.sh
@@ -4,4 +4,4 @@ git filter-branch --env-filter 'if [ "$GIT_AUTHOR_EMAIL" = "thiago.gaia@monetizz
 GIT_AUTHOR_EMAIL="thiagogaia@gmail.com";
 GIT_AUTHOR_NAME="Thiago Gaia"
 GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL;
-GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"; fi' -- --all
+GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"; fi' -- --all -f
